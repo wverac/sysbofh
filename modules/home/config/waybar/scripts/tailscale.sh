608022e773a6cc7c32fa8792e7e6a9170eb3f23d
William Vera <wv@linux.com>
2024-07-24 15:56:37 -0500

    added tooltip with exit-node info to waybar tailscale icon

--- modules/home/config/waybar/scripts/tailscale.sh
+++ modules/home/config/waybar/scripts/tailscale.sh
@@ -2,11 +2,11 @@
 
 if tailscale status &>/dev/null; then
   if tailscale exit-node list | grep -q selected; then
-    echo '{"text": " ", "class": "vpn-on"}'
+    tooltip=$(tailscale exit-node list | grep selected | awk '{print $2" "$3}')
+    echo "{\"text\": \" \", \"class\": \"vpn-on\", \"tooltip\": \"$tooltip\"}"
   else
     echo '{"text": " ", "class": "vpn-off"}'
   fi
 else
   echo '{"text": " ", "class": "vpn-off"}'
 fi
-
--- modules/home/vim.nix
+++ modules/home/vim.nix
@@ -2,45 +2,45 @@
   programs.vim = {
     enable = true;
     extraConfig = ''
-            " Define a function and keybind to run shellcheck in a new buffer
-            function! RunShellCheck()
-            write
+      " Define a function and keybind to run shellcheck in a new buffer
+      function! RunShellCheck()
+      write
 
-            let l:output = system('shellcheck ' . expand('%'))
+      let l:output = system('shellcheck ' . expand('%'))
 
-            if empty(l:output)
-              echo "ShellCheck: OK"
-            else
-            rightbelow vsplit
-            enew
-            setlocal buftype=nofile
-            setlocal bufhidden=hide
-            setlocal noswapfile
-            setlocal filetype=sh
-            file ShellCheck
+      if empty(l:output)
+        echo "ShellCheck: OK"
+      else
+      rightbelow vsplit
+      enew
+      setlocal buftype=nofile
+      setlocal bufhidden=hide
+      setlocal noswapfile
+      setlocal filetype=sh
+      file ShellCheck
 
-            call append(0, split(l:output, "\n"))
-              endif
-            endfunction
+      call append(0, split(l:output, "\n"))
+        endif
+      endfunction
 
-            nnoremap <leader>sc :call RunShellCheck()<CR>
-            " End ShellCheck setup
+      nnoremap <leader>sc :call RunShellCheck()<CR>
+      " End ShellCheck setup
 
-            "set number
-            set tabstop=4
-            set shiftwidth=4
-            set expandtab
-            set ruler
-            set showmatch
-            set showmode
-            set esckeys
-            set nocompatible
-            set backspace=indent,eol,start
-            set nomodeline
-            set hlsearch
-            set mouse-=a
-            set pastetoggle=<F2>
-            nnoremap <leader>a :%!alejandra -qq<CR>
+      "set number
+      set tabstop=4
+      set shiftwidth=4
+      set expandtab
+      set ruler
+      set showmatch
+      set showmode
+      set esckeys
+      set nocompatible
+      set backspace=indent,eol,start
+      set nomodeline
+      set hlsearch
+      set mouse-=a
+      set pastetoggle=<F2>
+      nnoremap <leader>a :%!alejandra -qq<CR>
     '';
   };
   home.sessionVariables = {
