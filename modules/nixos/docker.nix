987e8dc50878efad3c16143ef7bc79e6459807e2
William Vera <wv@linux.com>
2024-10-18 23:13:32 -0500

    docker module bind to privileged ports

--- modules/nixos/docker.nix
+++ modules/nixos/docker.nix
@@ -9,4 +9,12 @@
     enable = true;
     setSocketVariable = true;
   };
+  security.wrappers = {
+    docker-rootlesskit = {
+      owner = "root";
+      group = "root";
+      capabilities = "cap_net_bind_service+ep";
+      source = "${pkgs.rootlesskit}/bin/rootlesskit";
+    };
+  };
 }
