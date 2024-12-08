ab1fa931a9ea49e0775a9085a9f3e0c5f549280d
William Vera <wv@linux.com>
2024-12-08 07:38:15 -0600

    nixlab: hydra bump to unstable

--- modules/nixos/hydra-service.nix
+++ modules/nixos/hydra-service.nix
@@ -1,10 +1,10 @@
 {...}: {
   config = {
-    nix.settings.allowed-uris = [
-      "github:"
-      "git+https://github.com/"
-      "git+ssh://github.com/"
-    ];
+    # nix.settings.allowed-uris = [
+    #   "github:"
+    #   "git+https://github.com/"
+    #   "git+ssh://github.com/"
+    # ];
     services.hydra = {
       enable = true;
       hydraURL = "http://localhost:3000";
@@ -19,6 +19,11 @@
         # Lab
         #  iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3000
       '';
+    };
+    nix = {
+      settings = {
+        restrict-eval = false;
+      };
     };
   };
 }
