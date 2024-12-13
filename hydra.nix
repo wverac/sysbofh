7f0591c22d8ef19c58276b743b6894c7da9ab8db
William Vera <wv@linux.com>
2024-12-13 16:38:54 -0600

    Hydra: dont check pkgs

--- hydra.nix
+++ hydra.nix
@@ -4,21 +4,9 @@
 }: let
   inherit (inputs.nixpkgs) lib;
 
-  notBroken = pkg: !(pkg.meta.broken or false);
-  isDistributable = pkg: (pkg.meta.license or {redistributable = true;}).redistributable;
-  hasPlatform = sys: pkg: lib.elem sys (pkg.meta.platforms or [sys]);
-  filterValidPkgs = sys: pkgs:
-    lib.filterAttrs (_: pkg:
-      lib.isDerivation pkg
-      && hasPlatform sys pkg
-      && notBroken pkg
-      && isDistributable pkg)
-    pkgs;
-
   getHomeCfg = _: cfg: cfg.config.home.activationPackage;
   getConfigTopLevel = _: cfg: cfg.config.system.build.toplevel;
 in {
-  pkgs = lib.mapAttrs filterValidPkgs outputs.packages;
   hosts = lib.mapAttrs getConfigTopLevel outputs.nixosConfigurations;
   homes = lib.mapAttrs getHomeCfg outputs.homeConfigurations;
 }
