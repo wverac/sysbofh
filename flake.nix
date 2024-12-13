50fd0dea1525f086347af6c967303d62b8006d78
William Vera <wv@linux.com>
2024-12-13 01:34:11 -0600

    hydra and flake update

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
   } @ inputs: let
     inherit (self) outputs;
   in {
-    hydraJobs = import ./hydra.nix {inherit inputs outputs;};
+    hydraJobs = {inherit inputs outputs;};
 
     nixosConfigurations = {
       # Home lab - Beelink S12 Pro Mini PC
