ae4ce81f0a2d4e718e0dc5fd8be07c77cab0bae7
William Vera <wv@linux.com>
2024-12-16 01:13:04 -0600

    nixlab: hydra dirty hack

--- modules/nixos/hydra-service.nix
+++ modules/nixos/hydra-service.nix
@@ -5,9 +5,12 @@
         "github:"
         "git+https://github.com/"
         "git+ssh://github.com/"
+        "git+ssh://github.com/wverac/"
+        "https://github.com/NixOS/"
       ];
       restrict-eval = false;
     };
+
     services.hydra = {
       enable = true;
       hydraURL = "http://localhost:3000";
@@ -15,12 +18,13 @@
       buildMachinesFiles = [];
       useSubstitutes = true;
     };
+
     networking.firewall = {
       enable = true;
       allowedTCPPorts = [8888 3000];
       extraCommands = ''
         # Lab
-        #  iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3000
+        # iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3000
       '';
     };
   };
