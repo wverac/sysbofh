2d43624801788175912b035d1d74f33fef2cf492
William Vera <wv@linux.com>
2024-12-26 10:49:40 -0600

    new docking station

--- modules/nixos/poweroptions.nix
+++ modules/nixos/poweroptions.nix
@@ -2,9 +2,13 @@
   services.logind = {
     powerKey = "suspend";
     powerKeyLongPress = "poweroff";
-    lidSwitch = "suspend-then-hibernate";
-    lidSwitchExternalPower = "suspend";
-    lidSwitchDocked = "suspend-then-hibernate";
+    # NOTE: ignore lidSwitch, testing docking station
+    # lidSwitch = "suspend-then-hibernate";
+    # lidSwitchExternalPower = "suspend";
+    # lidSwitchDocked = "suspend-then-hibernate";
+    lidSwitch = "ignore";
+    lidSwitchExternalPower = "ignore";
+    lidSwitchDocked = "ignore";
     extraConfig = ''
     '';
   };
