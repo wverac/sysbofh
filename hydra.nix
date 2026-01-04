8fd0eff6f38a9f93c9085c4022dd12561638c787
William Vera <wv@linux.com>
2026-01-03 21:57:28 -0600

    hydra: filter out darwin homeConfigurations from builds

--- hydra.nix
+++ hydra.nix
@@ -6,7 +6,12 @@
 
   getHomeCfg = _: cfg: cfg.config.home.activationPackage;
   getConfigTopLevel = _: cfg: cfg.config.system.build.toplevel;
+
+  # Filter out darwin configurations (Hydra only has Linux builders)
+  linuxHomes = lib.filterAttrs
+    (_: cfg: cfg.pkgs.stdenv.hostPlatform.isLinux)
+    outputs.homeConfigurations;
 in {
   hosts = lib.mapAttrs getConfigTopLevel outputs.nixosConfigurations;
-  homes = lib.mapAttrs getHomeCfg outputs.homeConfigurations;
+  homes = lib.mapAttrs getHomeCfg linuxHomes;
 }
