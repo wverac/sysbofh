c3ca12e093c9791a11c607b61c7b3854f38b68c6
William Vera <wv@linux.com>
2026-01-07 17:13:39 -0600

    sysbofh/nixlab: fixed nix-gc date format from 1w to 7d

--- hosts/nixlab/configuration.nix
+++ hosts/nixlab/configuration.nix
@@ -140,7 +140,7 @@
   nix.gc = {
     automatic = true;
     dates = "weekly";
-    options = "--delete-older-than 1w";
+    options = "--delete-older-than 7d";
   };
 
   # Optimize storage
--- hosts/sysbofh/configuration.nix
+++ hosts/sysbofh/configuration.nix
@@ -136,7 +136,7 @@
   nix.gc = {
     automatic = true;
     dates = "weekly";
-    options = "--delete-older-than 1w";
+    options = "--delete-older-than 7d";
   };
 
   # Optimize storage
