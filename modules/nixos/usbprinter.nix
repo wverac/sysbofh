ebc673f9cd6ca7fb6458cb924da071b99db2b1f3
William Vera <wv@linux.com>
2026-01-12 01:20:50 -0600

    sysbofh: remove cnijfilter2 C23 workaround now fixed upstream

--- modules/nixos/usbprinter.nix
+++ modules/nixos/usbprinter.nix
@@ -1,13 +1,6 @@
 {pkgs, ...}: {
   # Printing service for Canon Pixma TS202 USB
   services.printing.enable = true;
-  # services.printing.drivers = [pkgs.cnijfilter2];
-  # TODO: temporary workaround for C23 compatibility issue where 'bool' is now a keyword
-  # Remove this override once cnijfilter2 is fixed upstream in nixpkgs
-  services.printing.drivers = [
-    (pkgs.cnijfilter2.overrideAttrs (old: {
-      env.NIX_CFLAGS_COMPILE = toString (old.env.NIX_CFLAGS_COMPILE or "") + " -std=gnu17";
-    }))
-  ];
+  services.printing.drivers = [pkgs.cnijfilter2];
   environment.systemPackages = with pkgs; [cups];
 }
