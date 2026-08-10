3b9bb8a30b4b50fdd6d11e5bc99fc8d82a954295
William Vera <wv@linux.com>
2026-08-10 13:45:39 -0500

    sysbofh: update NoMachine to 9.8.2 / flake update

--- overlays/nomachine.nix
+++ overlays/nomachine.nix
@@ -1,10 +1,10 @@
 # Pin NoMachine to the current upstream tarball while nixpkgs catches up.
 final: prev: {
   nomachine-client = prev.nomachine-client.overrideAttrs (_old: {
-    version = "9.5.7";
+    version = "9.8.2";
     src = prev.fetchurl {
-      url = "https://download.nomachine.com/download/9.5/Linux/nomachine_9.5.7_2_x86_64.tar.gz";
-      hash = "sha256-8f4ZL3Ko5VunojXLvTS9P3oB+ZVCSYIA0GIjM8VpUO4=";
+      url = "https://download.nomachine.com/download/9.8/Linux/nomachine_9.8.2_1_x86_64.tar.gz";
+      hash = "sha256-bpXYeErjx4+soxsSI6EMO0lfFUEJAc5YT3ZIfkTdRBo=";
     };
   });
 }
